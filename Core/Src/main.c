b00bba2ce9d027fc1fb6429edc81e08a0190c3ab
Kacper Lasota <56150911+kasperro3@users.noreply.github.com>
2026-08-06 16:58:58 +0200

    Added dma support for adc

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "adc.h"
 #include "can.h"
+#include "dma.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -88,6 +89,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_CAN_Init();
   MX_USART1_UART_Init();
   MX_ADC1_Init();
