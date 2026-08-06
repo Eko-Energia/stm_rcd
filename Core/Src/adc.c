418c1053953c397babe72cc68611c2dbc3192be8
Kacper Lasota <56150911+kasperro3@users.noreply.github.com>
2026-08-06 18:53:37 +0200

    Revert "Enabled dma requests"
    This reverts commit 1e72074050f9b78e35b85331de0a257f2c280ec7.

--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -54,7 +54,7 @@ void MX_ADC1_Init(void)
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = ENABLE;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
   hadc1.Init.LowPowerAutoWait = DISABLE;
   hadc1.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
